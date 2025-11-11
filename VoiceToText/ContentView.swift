28ecb36f3f03e8acff772ee1fe55645d20ae5fe9
Zhuocheng Yu <zhuocheng.yu.d@gmail.com>
2025-11-10 20:07:12 -0800

    Initial Commit

---
+++ VoiceToText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceToText
+//
+//  Created by Zhuocheng Yu on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceToText/VoiceToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceToTextApp.swift
+//  VoiceToText
+//
+//  Created by Zhuocheng Yu on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
